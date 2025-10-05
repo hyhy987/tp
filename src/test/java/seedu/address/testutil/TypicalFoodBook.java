d4c475e028a9ca9263d2e22e9c32d86120cc6ba5
hyhy987 <sie.haoyang@gmail.com>
2025-10-06 02:23:16 +0800

    Add storage of delivery objects

--- src/test/java/seedu/address/testutil/TypicalFoodBook.java
+++ src/test/java/seedu/address/testutil/TypicalFoodBook.java
@@ -1,33 +1,71 @@
 package seedu.address.testutil;
 
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+
+import java.util.List;
+
 import seedu.address.model.AddressBook;
+import seedu.address.model.delivery.DateTime;
+import seedu.address.model.delivery.Delivery;
 import seedu.address.model.person.Person;
 
 /**
- * A utility class containing a typical FoodBook to be used in tests.
+ * A utility class containing a list of {@code Person} and {@code Delivery} objects to be used in tests.
  */
 public class TypicalFoodBook {
-
-    private TypicalFoodBook() {} // prevents instantiation
-
-    // TODO: Once JSON serialization is implemented for delivveries, add typical deliveries here
-
-
-    /**
-     * Returns an {@code FoodBook} with all the typical persons and deliveries.
-     */
     public static AddressBook getTypicalAddressBook() {
         AddressBook ab = new AddressBook();
-        for (Person person : TypicalPersons.getTypicalPersons()) {
-            ab.addPerson(person);
+        for (Person p : getTypicalPersons()) {
+            ab.addPerson(p);
         }
-        /*
-        TODO: UNCOMMENT ONCE JSON SERIALIZATION IS IMPLEMENTED FOR DELIVERIES
-        for (Delivery delivery : TypicalDeliveries.getTypicalDeliveries()) {
-            ab.addDelivery(delivery);
-        }
-        */
         return ab;
     }
 
+    /**
+     * Returns an AddressBook with typical persons and typical deliveries.
+     */
+    public static AddressBook getTypicalAddressBookWithDeliveries() {
+        AddressBook ab = getTypicalAddressBook();
+        for (Delivery d : getTypicalDeliveries(ab)) {
+            ab.addDelivery(d);
+        }
+        return ab;
+    }
+
+    /**
+     * Returns a list of typical Person objects.
+     */
+    public static List<Person> getTypicalPersons() {
+        return List.of(ALICE, BENSON, CARL); // import from your existing TypicalPersons
+    }
+
+    /**
+     * Returns a list of typical Delivery objects, each linked to a person in the provided AddressBook.
+     *
+     * @param ab The address book containing the clients for lookup.
+     */
+    public static List<Delivery> getTypicalDeliveries(AddressBook ab) {
+        try {
+            // Find Alice in the book
+            Person alice = ab.getPersonList().stream()
+                    .filter(p -> p.getName().fullName.equals("Alice Yeoh"))
+                    .findFirst()
+                    .orElseThrow();
+            DateTime dt1 = new DateTime("15/10/2025", "0930");
+            Delivery d1 = new Delivery(1, alice, dt1, "Breakfast order", 30.50);
+
+            Person benson = ab.getPersonList().stream()
+                    .filter(p -> p.getName().fullName.equals("Benson Meier"))
+                    .findFirst()
+                    .orElseThrow();
+            DateTime dt2 = new DateTime("16/10/2025", "1200");
+            Delivery d2 = new Delivery(2, benson, dt2, "Lunch catering", 100.00);
+
+            return List.of(d1, d2);
+        } catch (Exception e) {
+            throw new AssertionError("Typical deliveries setup failed", e);
+        }
+    }
 }
