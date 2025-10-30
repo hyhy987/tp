def905b8a81f59054b81d60428d951e020bde2c0
Sai <saibhagatsr@gmail.com>
2025-10-30 17:18:43 +0800

    Add developer guide

--- src/main/java/seedu/foodbook/ui/MainWindow.java
+++ src/main/java/seedu/foodbook/ui/MainWindow.java
@@ -206,10 +206,6 @@ public class MainWindow extends UiPart<Stage> {
         primaryStage.hide();
     }
 
-    public PersonListPanel getPersonListPanel() {
-        return personListPanel;
-    }
-
     /**
      * Executes the command and returns the result.
      *
