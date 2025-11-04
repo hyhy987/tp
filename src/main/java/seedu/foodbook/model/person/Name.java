9909d08c19418e3f1261bd6ddf0786bee4daac48
Izzat <mizzat03@gmail.com>
2025-11-04 12:40:49 +0800

    Fixed checkstyle issues

--- src/main/java/seedu/foodbook/model/person/Name.java
+++ src/main/java/seedu/foodbook/model/person/Name.java
@@ -10,14 +10,16 @@ import static seedu.foodbook.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces " +
-                    "with special exception of / used only in s/o, d/o, S/O, or D/O. Additionally, it should not be blank";
+            "Names should only contain alphanumeric characters and spaces "
+                    + "with special exception of / used only in s/o, d/o, S/O, or D/O."
+                    + " Additionally, it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^[\\p{Alnum}]+( ([sS]/[oO]|[dD]/[oO]) [\\p{Alnum}]+)*[\\p{Alnum} +]*$";
+    public static final String VALIDATION_REGEX = "^[\\p{Alnum}]+( ([sS]/[oO]|[dD]/[oO])"
+            + " [\\p{Alnum}]+)*[\\p{Alnum} +]*$";
 
     public final String fullName;
 
